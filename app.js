f0debdf678b9ea511fe397b386eb01df0f944e70
njanfang <njanfang@gmail.com>
2024-04-18 11:26:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();app.get('/', function (req, res) {
-  res.send('it s very exciting to do it njanfang!!10');
+  res.send('it is very exciting to do devops njanfang!!!');
 });app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
